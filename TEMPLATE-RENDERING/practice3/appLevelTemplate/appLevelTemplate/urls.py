e1ee121564a7a1c58a1f64670ce6067ec3e41fc6
Mohaideen27 <mohaideenmech27@gmail.com>
2026-06-11 11:08:30 +0530

    PRACTICING  DJANGO TEMPLATE RENDERING

---
+++ TEMPLATE-RENDERING/practice3/appLevelTemplate/appLevelTemplate/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include('home.urls')),
+    path("",include('app1.urls')),
+    path("",include('app2.urls')),
+    path("",include('app3.urls')),
+]
